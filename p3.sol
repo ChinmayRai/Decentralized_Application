d46bf4246516dd1ad0a29e2ff34dd63e0f523625
ChinmayRai <cs5160615@iitd.ac.in>
2018-12-15 12:48:50 +0900

    Final Commit

--- p3.sol
+++ p3.sol
@@ -3,8 +3,8 @@ pragma solidity ^0.4.18;
 contract Parking {
 	
 	struct parkingSpot{
+		string name;
 	    address owner;
-		mapping (uint => uint) bookingIndex;
 		uint numBooking;
 		int lat;
 		int long;
@@ -21,53 +21,62 @@ contract Parking {
 	}
 
 	struct userData {
-		mapping (uint => uint) parkingSpotIndex;
 		uint numParkingSpots;
-		mapping (uint => uint) bookingIndex;
+		uint userNum;
 		uint numBooking;
 	}
 	
     address public overallOwner;
 	mapping (address => string) public userPass;
-	mapping (address => string) public userAddress;
+	mapping (bytes32 => address) public userAddress;
 	mapping (address => userData) public userAcc; 
 	parkingSpot [] public spots;
 	booking [] public bookings;
 	uint public numSpots;  // traverse lots using this because of logical deletion
 	uint public numBooking;
+	uint public numUsers;
+	uint[6][] public spotbookingIndex;
+	uint[6][] public userSpotIndex;
+	uint[6][] public userBookingIndex;
+	
 
 	//constructor
 	function Parking() public {
 		numBooking = 0;
 		numSpots = 0;
+		numUsers=0;
 		overallOwner = msg.sender;
 	}
 
 	//changes state
-   	function signUp (string username, string password, address account) public returns (bool){
-   		userPass[account] = password;
-   		userAddress[account] = username;
-   		userAcc[account]=userData({numParkingSpots: 0, numBooking: 0});
+   	function signUp (string _username, string _password, address _account) public returns (bool){
+   		userPass[_account] = _password;
+   		userAddress[keccak256(_username)] = _account;
+   		userAcc[_account]= userData(0,numUsers,0);
+   		userBookingIndex.push([32,32,32,32,32,32]);
+   		userSpotIndex.push([32,32,32,32,32,32]);
+   		numUsers++;
    		return true;
    	}
    	
 
-	function addRentOutSpot(address owner, int lat, int long, uint price) public returns (bool){
-		spots.push(parkingSpot({owner:owner,  numBooking:0, lat:lat, long:long, price:price}));
-		userAcc[owner].parkingSpotIndex[userAcc[owner].numParkingSpots]=numSpots;
-		userAcc[owner].numParkingSpots++;
+	function addRentOutSpot(string _name, address _owner, int _lat, int _long, uint _price) public returns (bool){
+		spots.push(parkingSpot(_name, _owner, 0, _lat, _long, _price));
+		spotbookingIndex.push([32,32,32,32,32,32]);
+		userSpotIndex[userAcc[_owner].userNum][userAcc[_owner].numParkingSpots]=numSpots;
+		userAcc[_owner].numParkingSpots++;
         numSpots++;
         return true;
 	}
 
 	// i = index of spot
-	function reserveSpot(uint i, uint start, uint stop) public payable returns (bool){
-		if(msg.value>=spots[i].price*(stop-start)/(3600) ){
-			if(checkAvailability(i, start, stop)){
-				bookings.push(booking(i, msg.sender, true, start, stop, msg.value));
-				spots[i].bookingIndex[spots[i].numBooking] = numBooking;
-				spots[i].numBooking++;
-				userAcc[msg.sender].bookingIndex[userAcc[msg.sender].numBooking]=numBooking;
+	function reserveSpot(uint _i, uint _start, uint _stop) public payable returns (bool){
+		if(msg.value>=(spots[_i].price*(_stop-_start)/(3600))){
+			if(checkAvailability(_i, _start, _stop)){
+				bookings.push(booking(_i, msg.sender, true, _start, _stop, msg.value));
+				spotbookingIndex[_i][spots[_i].numBooking] = numBooking;
+				spots[_i].numBooking++;
+				userBookingIndex[userAcc[msg.sender].userNum][userAcc[msg.sender].numBooking]=numBooking;
 				userAcc[msg.sender].numBooking++;
 				numBooking++;
 				return true;
@@ -78,23 +87,28 @@ contract Parking {
 	}
 
 	// index refers to bookingIndex of userData
-	function abandonSpot(address account, uint index) public returns (bool) { 
-		if (index >= userAcc[account].numBooking) return false;
-		uint n = userAcc[account].bookingIndex[index];
+	function abandonSpot(address _account, uint _index) public returns (bool) { 
+		if (_index >= userAcc[_account].numBooking) return false;
+		userData memory d = userAcc[_account];
+		uint n = userBookingIndex[d.userNum][_index];
 		bookings[n].active=false;
 		address owner = spots[bookings[n].spotId].owner;
-		owner.transfer(bookings[n].price);
+		owner.transfer(bookings[n].price );
 	}
     
     //private function 
-	function checkAvailability(uint n, uint start, uint stop) private view returns (bool) {
-		for (uint i=0; i<spots[n].numBooking; i++){
-			booking memory b = bookings[spots[n].bookingIndex[i]];
-			if(b.start < start && start < b.end ) return false;
-			else if (b.start < stop && stop < b.end ) return false;
-			else if (b.start > start && b.end < stop ) return false;
+	function checkAvailability(uint _n, uint _start, uint _stop) private view returns (bool) {
+		for (uint i=0; i<spots[_n].numBooking; i++){
+			booking memory b = bookings[spotbookingIndex[_n][i]];
+			if(b.start < _start && _start < b.end ) return false;
+			else if (b.start < _stop && _stop < b.end ) return false;
+			else if (b.start > _start && b.end < _stop ) return false;
 		}
 		return true;
+	}
+
+	function hash(string _s) public pure returns (bytes32){
+		return keccak256(_s);
 	}
     
     //deletes the contract  	
